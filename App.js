4478ec4fb77f2b59abb50b23306c3e753bc3d483
Huse Kivrak <huse7@pm.me>
2023-05-23 14:46:06 -0400

    tested WebView

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 import Login from './Login';
 
 export default function App() {
   const [sessionId, setSessionId] = useState("");
 
+  /** rithmLogin
+   *
+   * store sessionId/token from rithm api?
+   *
+   */
   async function rithmLogin(data){
     /**make POST request to rithm API with login credentials,
      *store sessionId/cookie somewhere, use for future requests*/
