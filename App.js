34d1995816e875611470f74a06f0d556f272dbdc
Alyssa Denary <alyssa.denary@gmail.com>
2023-05-24 14:02:15 -0400

    add: print statements

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-[import { StatusBar } from "expo-status-bar";
+import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { useState, useEffect } from "react";
-import RithmApi from "./api.js";
+import RithmApi from "./api";
 import Login from "./Login";
 import HomePage from "./HomePage";
+import { getToken } from "./secureStore";
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
+  console.log("App launched with loggedIn = ", loggedIn);
 
   useEffect(
     function checkAndSetLoginStatusOnMount() {
@@ -14,8 +16,10 @@ export default function App() {
         try {
           await getToken();
           setLoggedIn(true);
+          console.log("Check login on mount complete")
         } catch (err) {
           setLoggedIn(false);
+          console.log("Check login on mount - caught error")
         }
       }
       checkSetLogin()
@@ -30,19 +34,20 @@ export default function App() {
 async function rithmLogin(data) {
   /**make POST request to rithm API with login credentials,
    *store sessionId/cookie somewhere, use for future requests*/
+  console.log("rithmLogin(App) called with data = ", data);
 
   const success = await RithmApi.login(data);
   if (success === "Token saved.") {
     setLoggedIn(true);
   }
+}
 
-  return (
-    <View style={styles.container}>
-      {loggedIn ? <HomePage /> : <Login rithmLogin={rithmLogin} />}
-      <StatusBar />
-    </View>
-
-  );
+return (
+  <View style={styles.container}>
+    {loggedIn ? <HomePage /> : <Login rithmLogin={rithmLogin} />}
+    <StatusBar />
+  </View>
+);
 }
 
 const styles = StyleSheet.create({
@@ -53,4 +58,3 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
-}
