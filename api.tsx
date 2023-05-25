57dcde1e3162a12913c29cc702d8599c21664cc8
Huse Kivrak <huse7@pm.me>
2023-05-24 20:33:50 -0400

    troubleshooting server request

--- api.tsx
+++ api.tsx
@@ -38,7 +38,9 @@ class RithmApi {
       //   });
       // res = await axios.post("http://127.0.0.1:8000/api/-token/", cred);
       // res = await axios.post("http://10.0.2.2:8000/api/-token/", cred);
-      res = await axios.post(`https://${process.env.MACHINE_IP_ADDRESS}:8000/api/-token/`, cred);
+      // res = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto")
+      // res = await axios.post("http://10.0.2.2:8000/api/-token/", cred);
+      res = await axios.post(`http://192.168.1.19:8000/api/-token/`, cred);
       // console.log("res = ", res);
       // console.log("Response success, res data = ", res.response.data);
     } catch (e) {
@@ -64,11 +66,11 @@ class RithmApi {
     // });
     // debugger;
 
+    console.log("res:", res)
+    // const token = res.data.token;
+    // console.log("token received = ", token);
 
-    const token = res.data.token;
-    console.log("token received = ", token);
-
-    await saveToken(token);
+    // await saveToken(token);
 
     return "Token saved.";
   }
