fb241c89486fda697ba578c74944fa853252367d
Huse Kivrak <huse7@pm.me>
2023-05-25 21:35:39 -0400

    FlatView added

--- HomePage.tsx
+++ HomePage.tsx
@@ -1,64 +1,126 @@
-import { Button, Text, View } from "react-native";
+import { Button, Text, View, FlatList, StyleSheet, StatusBar } from "react-native";
 import RithmApi from "./api";
 import { useEffect, useState } from "react";
 
 function HomePage({ logout }) {
   const [isLoading, setIsLoading] = useState(true);
-  const [date, setDate] = useState(new Date().toDateString());
-  const [dayItems, setDayItems] = useState(null);
+  const [curricItems, setCurricItems] = useState(null);
+  const [dayIndex, setDayIndex] = useState(0);
+  //swipeCount state = 0;
 
   useEffect(function getDayCurricItemsOnMount() {
     async function getDayCurricItems() {
       console.log("useEffect for HomePage called");
-      let items = await RithmApi.getDayCurric();
+      let items = await RithmApi.getCurricByDay();
       console.log("day items from Rithm API= ", items);
 
-      setDayItems(items);
+      setCurricItems(items);
+      console.log("items in state: ", items);
       setIsLoading(false);
     }
     getDayCurricItems();
-  }, [date]);
+  }, []);
 
-/** handleSwipe
- *
- * setDate to (date) => date + 1
- *
- */
+  useEffect(
+    function findAndSetDayIndexAfterMount() {
+      function findAndSetDayIndex() {
+        console.log("findAndSetDayIndex called");
+        const today = new Date().toDateString().split("T")[0];
 
-  function makeCards(dayItems){
-    for (const item of dayItems){
-      if (item.type === "lecture"){
+        const idx = curricItems.findIndex((item) =>
+          item[0].start_at.startsWith(today)
+        );
 
-      } else if (item.type === "exercise"){
-
-      }else if( item.type === "event"){
-
+        setDayIndex(idx);
       }
+      if (curricItems && curricItems.length) findAndSetDayIndex();
+    },
+    [curricItems]
+  );
 
-      }
-    }
 
-  // if (isLoading){
-  //   return (
-  //     <View>
-  //       <Text>Loading...</Text>
-  //     </View>
-  //   );
-  // }
+
+  function handleSwipeRight() {
+    setDayIndex((idx) => idx++);
+  }
+
+  function handleSwipeLeft() {
+    if (dayIndex > 0) setDayIndex((idx) => idx--);
+  }
+
+  function Item({ title, description, start_at }) {
+    return (
+      <View>
+        <Text>I AM AN ITEM</Text>
+        <Text>{title}</Text>
+        <Text>{description}</Text>
+        <Text>{start_at}</Text>
+      </View>
+    );
+  }
+
+
+  if (isLoading) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+        <Button
+          onPress={logout}
+          title="Logout"
+          color="#f194ff"
+          accessibilityLabel="a button to logout when pressed"
+        />
+      </View>
+    );
+  }
 
   return (
     <View>
       <Text>Home Page</Text>
-      {/* {{makeCards()}} */}
-
+      <FlatList
+        data={curricItems}
+        renderItem={({ item }) => (
+          <Item
+            title={item.title}
+            description={item.description}
+            start_at={item.start_at}
+          />
+        )}
+        keyExtractor={(item, idx) => idx}
+        horizontal
+      />
       <Button
         onPress={logout}
         title="Logout"
         color="#f194ff"
         accessibilityLabel="a button to logout when pressed"
       />
+      <StatusBar/>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+  description: {
+    fontSize: 18,
+  },
+  start_at: {
+    fontSize: 12,
+  }
+});
 
 export default HomePage;
