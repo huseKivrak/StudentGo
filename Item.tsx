b6a01af0cbed5febb3fe2b1143a1933e1cbe9806
Alyssa Denary <alyssa.denary@gmail.com>
2023-05-30 16:22:42 -0400

    refactor: styles to item

--- Item.tsx
+++ Item.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet
+} from "react-native";
 
 /** Item Component */
 function Item({ title, description, start_at }) {
   return (
     <View>
-      <Text>I AM AN ITEM</Text>
-      <Text>{title}</Text>
-      <Text>{description}</Text>
-      <Text>{start_at}</Text>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.description}>{description}</Text>
+      <Text style={styles.start_at}>{start_at}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 32,
+  },
+  description: {
+    fontSize: 18,
+  },
+  start_at: {
+    fontSize: 12,
+  },
+});
 
 export default Item;
