8ce8508f9a5c7d91d11ab17dd8c33291bc56ce6b
Huse Kivrak <huse7@pm.me>
2023-06-02 02:26:02 -0400

    DAY_MOD: show today, tomorrow, or upcoming accordingly

--- Item.tsx
+++ Item.tsx
@@ -1,16 +1,26 @@
-import {
-  View,
-  Text,
-  StyleSheet
-} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 /** Item Component */
 function Item({ title, description, start_at }) {
+  const today = new Date();
+  const tomorrow = new Date(today);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  const startDay = new Date(start_at);
+
+  const DAY_MOD =
+    startDay === today
+      ? "today"
+      : startDay === tomorrow
+      ? "tomorrow"
+      : "upcoming";
+
+  const startTime = new Date(start_at).toLocaleTimeString();
   return (
     <View>
+      <Text style={styles.DAY_MOD}>{DAY_MOD}</Text>
       <Text style={styles.title}>{title}</Text>
+      <Text style={styles.start_at}>{startTime}</Text>
       <Text style={styles.description}>{description}</Text>
-      <Text style={styles.start_at}>{start_at}</Text>
     </View>
   );
 }
@@ -23,6 +33,9 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   start_at: {
+    fontSize: 16,
+  },
+  DAY_MOD: {
     fontSize: 12,
   },
 });
